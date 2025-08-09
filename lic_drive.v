9dc2c1d0c318f4fb99a63cd54cf4789efb8205d0
JiaTu_Li <A2830850396@163.com>
2025-08-09 20:56:17 +0800

    完善iic_drive模块，添加读取数据的状态机逻辑，更新状态转换和信号处理

--- lic_drive.v
+++ lic_drive.v
@@ -16,8 +16,8 @@ module iic_drive(
 	output reg  				scl,
 	inout 						sda,
 	output reg 					busy,
-	output reg 					err
-		
+	output reg 					err,
+	output reg [7:0] 			rd_data
     );
 reg [  7:0]	nstate;
 reg [  7:0]	cstate;	
@@ -28,13 +28,15 @@ localparam  wr_dev_ctrl   	=  8'b1111_1011; //fb// 发送设备地址（写）
 localparam  wr_reg_high	    =  8'b1111_0111; //f7// 发送寄存器高字节
 localparam  wr_reg_low    	=  8'b1110_1111; //ef// 发送寄存器低字节
 localparam  wr_data_byte    =  8'b1101_1111; //df// 发送数据字节（写操作）
+
+
+localparam  repeat_start	=  8'b0111_1101; //7D// 重复 START 条件
+//localparam  rd_data_byte    =  8'b1001_1111; //9f// 读取数据字节（读操作）
+localparam  rd_dev_ctrl     =  8'b0110_1111; //6F// 发送设备地址（读）	
+//localparam  /*rd_reg_high*/     =  8'b0101_1111; //5f// 读取寄存器高字节
+//localparam  /*rd_reg_low*/      =  8'b0011_1111; //3f// 读取寄存器低字节
+
 localparam  i2c_over		=  8'b1011_1111; //bf// 传输结束
-
-//localparam  rd_data_byte    =  8'b1001_1111;  //9f// 读取数据字节（读操作）
-//localparam  rd_dev_ctrl    =  8'b0111_1111; //7f// 发送设备地址（读）	
-//localparam  rd_reg_high    =  8'b0101_1111;	 //5f// 读取寄存器高字节
-//localparam  rd_reg_low     =  8'b0011_1111; //3f// 读取寄存器低字节
-
 
 //localparam  i2c_ack    		=  8'b0111_1111;  //7f
 
@@ -42,6 +44,11 @@ reg [  7:0]	dev_r;
 reg [  7:0]	reg_h;
 reg [  7:0]	reg_l;
 reg [  7:0] data_byte_r;
+
+reg [  7:0] rd_dev_r;
+reg [  7:0] rd_reg_h;
+reg [  7:0] rd_reg_l;
+reg [  7:0] rd_data_byte_r;//存储写进来的数据
 
 reg 		sda_o;
 wire 		sda_i;
@@ -58,22 +65,29 @@ reg [ 15:0] Rec_count;
 	// .probe4					(register[7:0])
 // );
 
-always @(*) begin//状态机的一个转换
+always @(*) begin
     case (cstate)
 		idle:			nstate <=	(start_en)		? start_bit 	: idle;
 		start_bit:		nstate <=	(State_turn)	? wr_dev_ctrl	: start_bit;
 		wr_dev_ctrl:	nstate <=	(State_turn)	? wr_reg_high	: wr_dev_ctrl;
 		wr_reg_high:	nstate <=	(State_turn)	? wr_reg_low	: wr_reg_high;
-		wr_reg_low :	nstate <=	(State_turn)	? wr_data_byte	: wr_reg_low;
-		// 
+		//wr_reg_low :	nstate <=	(State_turn)	? wr_data_byte	: wr_reg_low;
+
+		wr_reg_low :	nstate <=	(State_turn)	? (wr_rd_flag)  ? repeat_start: wr_data_byte	: wr_reg_low;
 		wr_data_byte:	nstate <=	(State_turn)	? i2c_over		: wr_data_byte;
-		i2c_over:		nstate <=	(State_turn)	? idle			: i2c_over;
+
+		repeat_start:	nstate <=	(State_turn)	? rd_dev_ctrl	: repeat_start;
+		rd_dev_ctrl:	nstate <=	(State_turn)	? rd_data_byte	: rd_dev_ctrl;
+		rd_data_byte:	nstate <=	(State_turn)	? i2c_over		: rd_data_byte;
+		///*rd_reg_high*/:	nstate <=	(State_turn)	? /*rd_reg_low*/	: /*rd_reg_high*/;
+		///*rd_reg_low*/:		nstate <=	(State_turn)	? i2c_over		: /*rd_reg_low*/;
 		
+		i2c_over:		nstate <=	(State_turn)	? idle			: i2c_over;
 	endcase
 end
+
 //scl
-always @(posedge clk_i or negedge rst_n)
-begin
+always @(posedge clk_i or negedge rst_n) begin
 	if(!rst_n) begin
 		scl	<= 1'b1;
 	end
@@ -82,18 +96,31 @@ begin
 			idle:begin
 				scl	<= 1'b1;
 			end
-			start_bit:begin
+			start_bit,repeat_start:begin
 				if(Rec_count >= 16'd2)	scl	<= 1'b0;				
 				else 					scl	<= 1'b1;
 			end
-			wr_dev_ctrl,wr_reg_high,wr_reg_low,wr_data_byte:	scl	<= ~scl;
+
+			wr_dev_ctrl,
+			wr_reg_high,
+			wr_reg_low,
+			wr_data_byte,
+			rd_dev_ctrl,
+			/*rd_reg_high*/,
+			/*rd_reg_low*/,
+			rd_data_byte_r:	scl	<= ~scl;
+
 			i2c_over:begin
+				/*
 				if(Rec_count == 16'd0)							scl	<= ~scl;
 				else 											scl <= scl;
+				*/
+				scl <= 1'b1; //因为主机要控制stop
 			end
 		endcase 
 	end
 end
+
 //sda_t
 // always @(posedge clk_i or negedge rst_n)
 // begin
@@ -121,7 +148,8 @@ begin
 	else begin
 		case (nstate)
 			idle:			sda_t	<= 1'b1;
-			start_bit:		sda_t	<= 1'b0;	
+			start_bit,repeat_start:		sda_t	<= 1'b0;	
+			rd_data_byte_r:sda_t	<= 1'b1;
 			wr_dev_ctrl,wr_reg_high,wr_reg_low,wr_data_byte:begin
 				if(Rec_count ==16'd15 || Rec_count == 16'd16)begin
 					sda_t	<= 1'b1;
@@ -154,7 +182,7 @@ begin
 				data_byte_r <= data_byte_r;
 			end
 			start_bit:begin		
-				dev_r 		<= {i2c_device_addr[7:1] , wr_rd_flag};
+				dev_r 		<= {i2c_device_addr[7:1] , 1'b0 /*wr_rd_flag*/};
 				reg_h 		<= register[15:8];	
 				reg_l 		<= register[ 7:0];	
 				data_byte_r <= data_byte;	
@@ -165,6 +193,9 @@ begin
 					sda_o	<= 1'b0;
 				end
 			end
+
+		
+
 			wr_dev_ctrl:begin
 				reg_h 		<= reg_h;
 				reg_l 		<= reg_l;
@@ -182,6 +213,7 @@ begin
 					end
 				end
 			end
+
 			wr_reg_high:begin
 				dev_r 		<= dev_r;
 				reg_l 		<= reg_l;
@@ -233,11 +265,60 @@ begin
 					end
 				end			
 			end
+		
+
+
+			
+			repeat_start : begin
+				rd_dev_r       <= {dev_r[7:1],1'b1} ; //读操作时，设备地址的最后一位为1
+				rd_data_byte_r <= data_byte_r ;
+				if(Rec_count >=16'd3)begin
+					sda_o	<= dev_r[7];				        
+				end	
+				else begin
+					sda_o	<= 1'b0;
+				end
+			end
+			rd_dev_ctrl:begin
+				rd_dev_r       <= rd_dev_r;
+				rd_data_byte_r <= rd_data_byte_r ;		
+				if(Rec_count ==16'd15 || Rec_count == 16'd16)begin
+					sda_o	<= 1'b1;
+				end		
+				else if (Rec_count == 16'd17)begin
+					sda_o	<= rd_dev_r[7];
+				end				
+				else begin
+					sda_o	<= rd_dev_r[7];
+					if(!scl)begin
+						rd_dev_r <= {rd_dev_r[6:0],rd_dev_r[7]};
+					end
+				end
+			end
+		
+			rd_data_byte:begin
+				rd_dev_r       <= rd_dev_r;
+				rd_data_byte_r <= rd_data_byte_r ;		
+				if(Rec_count ==16'd15 || Rec_count == 16'd16)begin
+					sda_o	<= 1'b1;
+				end		
+				else if (Rec_count == 16'd17)begin
+					sda_o	<= 1'b0;
+				end				
+				else begin
+					sda_o	<= rd_data_byte_r[7];
+					if(!scl)begin
+						rd_data_byte_r <= {rd_data_byte_r[6:0],sda_i};
+					end
+				end
+			end
+
 			i2c_over:begin
 				dev_r 		<= dev_r;
 				reg_h 		<= reg_h;
 				reg_l 		<= reg_l;
 				data_byte_r <= data_byte_r;
+				rd_data	 <= rd_data_byte_r;
 				if(Rec_count <= 16'd1)begin
 					sda_o	<= 1'b0;
 				end
@@ -262,7 +343,7 @@ begin
 				Rec_count	<= 16'd0;
 				State_turn 	<=  1'b0;
 			end
-			start_bit,i2c_over:begin
+			start_bit,repeat_start,i2c_over:begin
 				if (Rec_count == 16'd4 - 1'b1) begin
 					Rec_count  	<= 16'd0;
 					State_turn  <=  1'b1;
@@ -272,7 +353,7 @@ begin
 					State_turn  <= 1'b0;
 				end				
 			end
-			wr_dev_ctrl,wr_reg_high,wr_reg_low,wr_data_byte:begin
+			wr_dev_ctrl,wr_reg_high,wr_reg_low,wr_data_byte,rd_dev_ctrl,rd_data_byte_r:begin
 				if (Rec_count == 16'd18 - 1'b1) begin
 					Rec_count  	<= 16'd0;
 					State_turn  <=  1'b1;
@@ -293,10 +374,10 @@ begin
 	end
 	else begin
 		case (nstate)
-			idle,i2c_over,start_bit:	begin
+			idle,i2c_over,start_bit,repeat_start:	begin
 				err	<= err;
 			end
-			wr_dev_ctrl,wr_reg_high,wr_reg_low,wr_data_byte:begin
+			wr_dev_ctrl,wr_reg_high,wr_reg_low,wr_data_byte,rd_dev_ctrl,rd_data_byte_r:begin
 				if(Rec_count==16'd16) begin
 					if(sda_i) 	err	<= 1'b1;
 					else 		err	<= 1'b0;
@@ -316,7 +397,7 @@ begin
 	else begin
 		case (nstate)
 			idle: busy	<= 1'b0;
-			wr_dev_ctrl,wr_reg_high,wr_reg_low,wr_data_byte,i2c_over,start_bit: busy	<= 1'b1;
+			wr_dev_ctrl,wr_reg_high,wr_reg_low,wr_data_byte,i2c_over,start_bit,repeat_start,rd_dev_ctrl,rd_data_byte_r: busy	<= 1'b1;
 		endcase 
 	end
 
